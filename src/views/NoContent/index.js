56d749a64ca9a5e7fbdd00a3e6e1ef4dad39007b
LiveSlowSkateFast <jp@jprowan.com>
2018-05-31 18:43:03 -0700

    Removed Sign Up link from NoContent

--- src/views/NoContent/index.js
+++ src/views/NoContent/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { EmptyState } from '@auth0/styleguide-react-components';
-import {
-    SignUpButton,
-    LogInButton
-} from "components";
+import { Link } from "react-router-dom";
 
-const NoContent = () => (
+const NoContent = (props) => {
+  const { auth } = props
+  return (
     <EmptyState
-        title="Well, this is pretty boring..."
-        description="There isn't a whole lot to do here until you log in."
-        iconCode="311">
-        <LogInButton />
-        <SignUpButton />
+      title="Well, this is pretty boring..."
+      description="There isn't a whole lot to do here until you log in."
+      iconCode="311">
+      <Link
+        to="/"
+        onClick={() => auth.login()}
+        className={props.className ? props.className : "btn btn-success"}>
+        Log In
+      </Link>
     </EmptyState>
-);
+  )
+}
 
 export default NoContent;
