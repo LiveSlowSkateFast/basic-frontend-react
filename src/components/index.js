47ede29a56729d30568991118b368f03b49ff480
LiveSlowSkateFast <jp@jprowan.com>
2018-05-31 18:46:32 -0700

    Remove Button Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,7 @@
-
-// ##############################
-// // // Buttons
-// #############################
-import LogInButton from "./Buttons/LogInButton.jsx";
-import SignUpButton from "./Buttons/SignUpButton.jsx";
-
-// ##############################
-// // // Headers
-// #############################
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 export {
     Header,
     Sidebar,
-    LogInButton,
-    SignUpButton,
 }
