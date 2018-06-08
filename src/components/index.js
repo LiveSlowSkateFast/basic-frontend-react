c5065444a03cf2e41f718956270697afeaad88ee
LiveSlowSkateFast <jp@jprowan.com>
2018-06-08 09:43:14 -0700

    Moved App into components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
+import App from "./App";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Spinner from "./Spinner";
 
 export {
+    App,
     Header,
     Sidebar,
     Spinner,
