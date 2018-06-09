f1cfd5e03daaa871549c0b598c08707fc73b2e65
LiveSlowSkateFast <jp@jprowan.com>
2018-06-08 17:06:11 -0700

    Added default height to root container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Header, Sidebar } from "components";
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import { Footer } from '@auth0/styleguide-react-components';
 import { Auth, history } from 'services';
 import {
@@ -17,17 +16,11 @@ import {
 } from 'views';
 
 
-const theme = createMuiTheme({
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      'Fakt',
-    ].join(','),
-    body1: {
-      fontSize: 14,
-    },
-  },
-})
+const styles = {
+  rootContainer: {
+    minHeight: 400
+  }
+}
 
 const auth = new Auth();
 
@@ -57,7 +50,7 @@ class App extends Component {
   }
 
   checkSession = () => {
-    auth.checkSession().catch((e)=>{}).finally(() =>
+    auth.checkSession().catch((e) => { }).finally(() =>
       auth.isAuthenticated() ? this.setState({ isLoggedIn: true }) : null
     )
   }
@@ -73,30 +66,28 @@ class App extends Component {
       <Router history={history}>
         <React.Fragment>
           <CssBaseline />
-          <MuiThemeProvider theme={theme}>
-            <Header auth={auth} />
-            <div className="container root-container">
-              <Sidebar auth={auth} />
-              <div className="col-xs-9 wrapper">
-                <Switch>
-                  <Route exact path="/" render={(props) =>
-                    rootPage(props)} />
-                  <Route path="/configuration" render={(props) =>
-                    <Configuration {...props} />} />
-                  <Route path="/token" render={(props) =>
-                    requiresAuth(<ViewToken {...props} />)} />
-                  <Route path="/profile" render={(props) =>
-                    requiresAuth(<Profile {...props} auth={auth} />)} />
-                  <Route path="/callback" render={(props) => {
-                    handleAuthentication(props);
-                    return <Callback {...props} />
-                  }} />
-                  <Route render={(props) => <NotFound {...props} />} />
-                </Switch>
-              </div>
+          <Header auth={auth} />
+          <div className="container root-container" style={styles.rootContainer}>
+            <Sidebar auth={auth} />
+            <div className="col-xs-9 wrapper">
+              <Switch>
+                <Route exact path="/" render={(props) =>
+                  rootPage(props)} />
+                <Route path="/configuration" render={(props) =>
+                  <Configuration {...props} />} />
+                <Route path="/token" render={(props) =>
+                  requiresAuth(<ViewToken {...props} />)} />
+                <Route path="/profile" render={(props) =>
+                  requiresAuth(<Profile {...props} auth={auth} />)} />
+                <Route path="/callback" render={(props) => {
+                  handleAuthentication(props);
+                  return <Callback {...props} />
+                }} />
+                <Route render={(props) => <NotFound {...props} />} />
+              </Switch>
             </div>
-            <Footer />
-          </MuiThemeProvider>
+          </div>
+          <Footer />
         </React.Fragment>
       </Router >
     )
--- src/views/Configuration/index.js
+++ src/views/Configuration/index.js
@@ -2,14 +2,8 @@ import React from "react";
 // import PropTypes from 'prop-types';
 import ScopesList from "./ScopesList";
 
-const styles = {
-  configurationContainer: {
-    minHeight: 400
-  }
-}
-
 const Configuration = () => (
-  <div className='configuration-container' style={styles.configurationContainer}>
+  <div className='configuration-container'>
     <h3> Select Scopes </h3>
     <ScopesList />
   </div>
