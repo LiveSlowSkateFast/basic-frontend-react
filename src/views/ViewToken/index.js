464966c814b69970ea6408a887e4387948f4933f
LiveSlowSkateFast <jp@jprowan.com>
2018-08-02 21:04:22 -0700

    token view shows content of access token

--- src/views/ViewToken/index.js
+++ src/views/ViewToken/index.js
@@ -6,12 +6,14 @@ import { Code } from "components";
 const ViewCode = () => {
   return (
     <React.Fragment>
-      <h3> Access Token </h3>
-      <Code className='actionscript' code={localStorage.getItem('access_token')} />
       <h3> ID Token </h3>
       <Code className="actionscript" code={localStorage.getItem('id_token')} />
       <h3> ID Token Contents</h3>
       <Code code={JSON.stringify(jwt_decode(localStorage.getItem('id_token')), null, 2)} />
+      <h3> Access Token </h3>
+      <Code className='actionscript' code={localStorage.getItem('access_token')} />
+      <h3> Access Token Contents</h3>
+      <Code code={JSON.stringify(jwt_decode(localStorage.getItem('access_token')), null, 2)} />
     </React.Fragment>
   )
 }
